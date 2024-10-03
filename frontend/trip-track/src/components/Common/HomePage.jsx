734f1cb6186340d1d314e205fcf850df2adecc1b
HareuBang <divjae7124@gmail.com>
2024-10-04 00:50:00 +0900

    feat: HomePage.jsx 임시 /페이지(홈) 페이지 생성 디렉토리 설계 시 없던 파일

---
+++ frontend/trip-track/src/components/Common/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+
+  return (
+    <div className="container mt-4">
+      <h2 className="mb-4">Welcome to Trip Track!</h2>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
