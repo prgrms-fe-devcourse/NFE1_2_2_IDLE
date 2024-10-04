b4ff8267f56299aeae0fa6642cc03b1298a6153a
Builter <builter251@gmail.com>
2024-10-04 10:43:06 +0900

    refactor: 알림 컴포넌트를 삭제했습니다.
    알림 컴포넌트는 main page/after sign in 브랜치에서 처리할 예정입니다.

--- frontend/trip-track/src/App.jsx
+++ frontend/trip-track/src/App.jsx
@@ -1,11 +1,8 @@
-import NotificationButton from "./components/Notification/NotificationButton";
 import PostDetailPage from "./pages/PostDetailPage";
 
 function App() {
   return (
-    <>
-      <NotificationButton />
-      
+    <>      
       <PostDetailPage />
     </>
   );
