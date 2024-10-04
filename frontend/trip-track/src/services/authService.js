440101fb14111b3d32c63a332d2211a35d0e0f38
HareuBang <divjae7124@gmail.com>
2024-10-04 12:35:17 +0900

    feat: authServics.js 사용자 관련 API(get / follow)

---
+++ frontend/trip-track/src/services/authService.js
@@ -0,0 +1,37 @@
+import axiosInstance from "./axiosInstance"; // axiosInstance로 변경
+
+export const getUsers = async () => {
+  const response = await axiosInstance.get("/users/get-users");
+  return response.data;
+};
+
+export const getUser = async (userId) => {
+  const response = await axiosInstance.get(`/users/${userId}`);
+  return response.data;
+};
+
+// 팔로우 API
+export const followUser = async (userId) => {
+  try {
+    const response = await axiosInstance.post("/follow/create", {
+      userId: userId, // 서버에 userId 전달
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error following user:", error);
+    throw error;
+  }
+};
+
+// 언팔로우 API
+export const unfollowUser = async (followId) => {
+  try {
+    const response = await axiosInstance.delete("/follow/delete", {
+      data: { id: followId }, // DELETE 요청에 follow 객체의 _id 전달
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error unfollowing user:", error);
+    throw error;
+  }
+};
