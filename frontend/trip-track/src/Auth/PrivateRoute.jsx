ab31203b610b45a85213df7484477a7e312a9144
HareuBang <divjae7124@gmail.com>
2024-10-04 14:19:17 +0900

    feat: PrivateRoute.jsx 특정 경로에 대해서 인증된 사용자만 접근할 수 있도록 하는 기능 디렉토리에 없던 파일 및 LoadUserFromLocalStoreage.jsx와 추후에 수정 가능성이 있는 파일입니다.

---
+++ frontend/trip-track/src/Auth/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { currentUserState } from "../../recoil/atom";
+
+const PrivateRoute = ({ children }) => {
+  const currentUser = useRecoilValue(currentUserState);
+
+  // 인증된 사용자가 없으면 로그인 페이지로 리디렉션
+  return currentUser ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
