c70f2b515e8ec8890dd07589a34c558f7abd45e0
HareuBang <divjae7124@gmail.com>
2024-10-04 15:29:34 +0900

    feat: postService.js 포스트 관련 API 전부 구현

--- frontend/trip-track/src/services/postService.js
+++ frontend/trip-track/src/services/postService.js
@@ -2,25 +2,31 @@ import axios from "axios";
 
 const API_HOST = "https://kdt.frontend.5th.programmers.co.kr:5008";
 
-// 포스트 생성
-export const createPost = async (postData, token) => {
-  const response = await axios.post(`${API_HOST}/posts`, postData, {
-    headers: {
-      Authorization: `Bearer ${token}`,
+// 특정 채널의 포스트 목록 가져오기
+export const getPostsByChannel = async (channelId, offset = 0, limit = 10) => {
+  const response = await axios.get(`${API_HOST}/posts/channel/${channelId}`, {
+    params: {
+      offset,
+      limit,
     },
   });
   return response.data;
 };
 
 // 특정 사용자의 포스트 목록 가져오기
-export const getPostsByAuthor = async (authorId) => {
-  const response = await axios.get(`${API_HOST}/posts/author/${authorId}`);
+export const getPostsByAuthor = async (authorId, offset = 0, limit = 10) => {
+  const response = await axios.get(`${API_HOST}/posts/author/${authorId}`, {
+    params: {
+      offset,
+      limit,
+    },
+  });
   return response.data;
 };
 
-// 포스트 수정하기
-export const updatePost = async (postId, updatedData, token) => {
-  const response = await axios.put(`${API_HOST}/posts/${postId}`, updatedData, {
+// 포스트 생성
+export const createPost = async (postData, token) => {
+  const response = await axios.post(`${API_HOST}/posts/create`, postData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
@@ -28,11 +34,30 @@ export const updatePost = async (postId, updatedData, token) => {
   return response.data;
 };
 
-// 포스트 삭제하기
-export const deletePost = async (postId, token) => {
-  const response = await axios.delete(`${API_HOST}/posts/${postId}`, {
+// 특정 포스트 상세 보기
+export const getPostById = async (postId) => {
+  const response = await axios.get(`${API_HOST}/posts/${postId}`);
+  return response.data;
+};
+
+// 내가 작성한 포스트 수정하기
+export const updatePost = async (postId, updatedData, token) => {
+  const response = await axios.put(`${API_HOST}/posts/update`, updatedData, {
     headers: {
       Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+// 내가 작성한 포스트 삭제하기
+export const deletePost = async (postId, token) => {
+  const response = await axios.delete(`${API_HOST}/posts/delete`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    data: {
+      id: postId,
     },
   });
   return response.data;
