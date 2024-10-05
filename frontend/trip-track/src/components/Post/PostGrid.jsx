522a68063778cdcde7298c7aee85eecacc73b9d4
sunglitter <kds0284@gmail.com>
2024-10-05 21:18:25 +0900

    feat: 포스트 카드, 포스트 카드 영역(그리드) 추가했습니다.

---
+++ frontend/trip-track/src/components/Post/PostGrid.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PostCard from './PostCard';
+import './PostGrid.css'; // 스타일 정의 추가
+
+const PostGrid = ({ searchQuery, posts }) => {
+  return (
+    <div className="post-grid-container">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <PostCard key={post._id} post={post} />
+        ))
+      ) : (
+        <p></p> // 검색 결과가 없을 때 공백 표시
+      )}
+    </div>
+  );
+};
+
+PostGrid.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default PostGrid;
